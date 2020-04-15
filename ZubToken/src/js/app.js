9f2e79e9846f5fe1fae8fb7eafa2bc356ca5e285
MZubairLohar <zubairgituser@gmail.com>
2020-04-15 14:13:35 +0500

    web3 added

--- ZubToken/src/js/app.js
+++ ZubToken/src/js/app.js
@@ -1,22 +1,36 @@
 App = {
-  // web3Provider: null,
-  // contracts: {},
-  // account: '0x0',
-  // loading: false,
-  // tokenPrice: 1000000000000000,
-  // tokensSold: 0,
-  // tokensAvailable: 750000,
+  web3Provider: null,
+  contracts: {},
+  account: '0x0',
+  loading: false,
+  tokenPrice: 1000000000000000,
+  tokensSold: 0,
+  tokensAvailable: 750000,
 
   
     init: function() {
     console.log("App initialization... ")
-  } 
-}  
-// $(function () {
-//   $(window) .load(function () {
-//     App.init();
-//   })
-// });
-$(document).ready(function() {
-  App.init();
+    return App.initWeb3();
+  }, 
+
+  initWeb3: function(){
+    if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already provided by Meta Mask.
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+    }
+    return App.initContracts();
+  },
+}
+$(function () {
+  $(window) .load(function () {
+    App.init();
+  })
 });
+// $(document).ready(function() {
+//   App.init();
+// });
