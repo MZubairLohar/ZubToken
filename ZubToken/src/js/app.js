8cf00ed2080463716582659f7cf5861726d4763e
MZubairLohar <zubairgituser@gmail.com>
2020-04-17 17:08:50 +0500

    app.js completion with functions

--- ZubToken/src/js/app.js
+++ ZubToken/src/js/app.js
@@ -7,13 +7,12 @@ App = {
   tokensSold: 0,
   tokensAvailable: 750000,
 
-  
-    init: function() {
-    console.log("App initialization... ")
+  init: function() {
+    console.log("App initialized...")
     return App.initWeb3();
-  }, 
+  },
 
-  initWeb3: function(){
+  initWeb3: function() {
     if (typeof web3 !== 'undefined') {
       // If a web3 instance is already provided by Meta Mask.
       App.web3Provider = web3.currentProvider;
@@ -25,12 +24,110 @@ App = {
     }
     return App.initContracts();
   },
+
+  initContracts: function() {
+    $.getJSON("/ZubTokenSale.json", function(ZubTokenSale) { //.json missing
+      App.contracts.ZubTokenSale = TruffleContract(ZubTokenSale);
+      App.contracts.ZubTokenSale.setProvider(App.web3Provider);
+      App.contracts.ZubTokenSale.deployed().then(function(ZubTokenSale) {
+        console.log("Zub Token Sale Address:", ZubTokenSale.address);
+      });
+    }).done(function() {
+      $.getJSON("/ZubToken.json", function(ZubToken) {  //.json missing
+        App.contracts.ZubToken = TruffleContract(ZubToken);
+        App.contracts.ZubToken.setProvider(App.web3Provider);
+        App.contracts.ZubToken.deployed().then(function(ZubToken) {
+          console.log("Zub Token Address:", ZubToken.address);
+        });
+
+        App.listenForEvents();
+        return App.render();
+      });
+    })
+  },
+
+  // Listen for events emitted from the contract
+  listenForEvents: function() {
+    App.contracts.ZubTokenSale.deployed().then(function(instance) {
+      instance.Sell({}, {
+        fromBlock: 0,
+        toBlock: 'latest',
+      }).watch(function(error, event) {
+        console.log("event triggered", event);
+        App.render();
+      })
+    })
+  },
+
+  render: function() {
+    if (App.loading) {
+      return;
+    }
+    App.loading = true;
+
+    var loader  = $('#loader');
+    var content = $('#content');
+
+    loader.show();
+    content.hide();
+
+    // Load account data
+    web3.eth.getCoinbase(function(err, account) {
+      if(err === null) {
+        App.account = account;
+        $('#accountAddress').html("Your Account: " + account);
+      }
+    })
+
+    // Load token sale contract
+    App.contracts.ZubTokenSale.deployed().then(function(instance) {
+      ZubTokenSaleInstance = instance;
+      return ZubTokenSaleInstance.tokenPrice();
+    }).then(function(tokenPrice) {
+      App.tokenPrice = tokenPrice;
+      $('.token-price').html(web3.fromWei(App.tokenPrice, "ether").toNumber());
+      return ZubTokenSaleInstance.tokensSold();
+    }).then(function(tokensSold) {
+      App.tokensSold = tokensSold.toNumber();
+      $('.tokens-sold').html(App.tokensSold);
+      $('.tokens-available').html(App.tokensAvailable);
+
+      var progressPercent = (Math.ceil(App.tokensSold) / App.tokensAvailable) * 100;
+      $('#progress').css('width', progressPercent + '%');
+
+      // Load token contract
+      App.contracts.ZubToken.deployed().then(function(instance) {
+        ZubTokenInstance = instance;
+        return ZubTokenInstance.balanceOf(App.account);
+      }).then(function(balance) {
+        $('.Zub-balance').html(balance.toNumber());
+        App.loading = false;
+        loader.hide();
+        content.show();
+      })
+    });
+  },
+
+  buyTokens: function() {
+    $('#content').hide();
+    $('#loader').show();
+    var numberOfTokens = $('#numberOfTokens').val();
+    App.contracts.ZubTokenSale.deployed().then(function(instance) {
+      return instance.buyTokens(numberOfTokens, {
+        from: App.account,
+        value: numberOfTokens * App.tokenPrice,
+        gas: 500000 // Gas limit
+      });
+    }).then(function(result) {
+      console.log("Tokens bought...")
+      $('form').trigger('reset') // reset number of tokens in form
+      // Wait for Sell event
+    });
+  }
 }
-$(function () {
-  $(window) .load(function () {
+
+$(function() {
+  $(window).load(function() {
     App.init();
   })
-});
-// $(document).ready(function() {
-//   App.init();
-// });
+})
